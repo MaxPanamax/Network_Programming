faff7838a646aea7a5465f0caeb3e1a912c69fe3
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-03-14 16:55:57 +0200

    Network_Programming

---
+++ 4. Домашняя работа Пономаренок М.Б/Домашнее задание 4/FormMain.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Task1_4
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.comboBoxContentSearch = new System.Windows.Forms.ComboBox();
+            this.labelSearchLine = new System.Windows.Forms.Label();
+            this.labelContentSearch = new System.Windows.Forms.Label();
+            this.textBoxSearchLine = new System.Windows.Forms.TextBox();
+            this.comboBoxSearchSystem = new System.Windows.Forms.ComboBox();
+            this.labelSearchSystem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Enabled = false;
+            this.buttonSearch.Location = new System.Drawing.Point(374, 73);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(147, 32);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Поиск в браузере";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxContentSearch
+            // 
+            this.comboBoxContentSearch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxContentSearch.FormattingEnabled = true;
+            this.comboBoxContentSearch.Items.AddRange(new object[] {
+            "Общий поиск",
+            "Поиск изображений"});
+            this.comboBoxContentSearch.Location = new System.Drawing.Point(483, 31);
+            this.comboBoxContentSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBoxContentSearch.Name = "comboBoxContentSearch";
+            this.comboBoxContentSearch.Size = new System.Drawing.Size(140, 21);
+            this.comboBoxContentSearch.TabIndex = 4;
+            // 
+            // labelSearchLine
+            // 
+            this.labelSearchLine.AutoSize = true;
+            this.labelSearchLine.Location = new System.Drawing.Point(14, 83);
+            this.labelSearchLine.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSearchLine.Name = "labelSearchLine";
+            this.labelSearchLine.Size = new System.Drawing.Size(99, 13);
+            this.labelSearchLine.TabIndex = 6;
+            this.labelSearchLine.Text = "Строка поиска:";
+            // 
+            // labelContentSearch
+            // 
+            this.labelContentSearch.AutoSize = true;
+            this.labelContentSearch.Location = new System.Drawing.Point(371, 34);
+            this.labelContentSearch.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelContentSearch.Name = "labelContentSearch";
+            this.labelContentSearch.Size = new System.Drawing.Size(105, 13);
+            this.labelContentSearch.TabIndex = 3;
+            this.labelContentSearch.Text = "Контент поиска:";
+            // 
+            // textBoxSearchLine
+            // 
+            this.textBoxSearchLine.Location = new System.Drawing.Point(128, 80);
+            this.textBoxSearchLine.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxSearchLine.Name = "textBoxSearchLine";
+            this.textBoxSearchLine.Size = new System.Drawing.Size(238, 20);
+            this.textBoxSearchLine.TabIndex = 5;
+            // 
+            // comboBoxSearchSystem
+            // 
+            this.comboBoxSearchSystem.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSearchSystem.FormattingEnabled = true;
+            this.comboBoxSearchSystem.Items.AddRange(new object[] {
+            "https://www.google.com/",
+            "https://yandex.ru/"});
+            this.comboBoxSearchSystem.Location = new System.Drawing.Point(152, 31);
+            this.comboBoxSearchSystem.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comboBoxSearchSystem.Name = "comboBoxSearchSystem";
+            this.comboBoxSearchSystem.Size = new System.Drawing.Size(195, 21);
+            this.comboBoxSearchSystem.TabIndex = 1;
+            // 
+            // labelSearchSystem
+            // 
+            this.labelSearchSystem.AutoSize = true;
+            this.labelSearchSystem.Location = new System.Drawing.Point(14, 34);
+            this.labelSearchSystem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSearchSystem.Name = "labelSearchSystem";
+            this.labelSearchSystem.Size = new System.Drawing.Size(130, 13);
+            this.labelSearchSystem.TabIndex = 2;
+            this.labelSearchSystem.Text = "Поисковая система:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(671, 165);
+            this.Controls.Add(this.labelContentSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearchLine);
+            this.Controls.Add(this.comboBoxContentSearch);
+            this.Controls.Add(this.labelSearchSystem);
+            this.Controls.Add(this.labelSearchLine);
+            this.Controls.Add(this.comboBoxSearchSystem);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormMain";
+            this.Text = "Поиск";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ComboBox comboBoxContentSearch;
+        private System.Windows.Forms.Label labelSearchLine;
+        private System.Windows.Forms.Label labelContentSearch;
+        private System.Windows.Forms.TextBox textBoxSearchLine;
+        private System.Windows.Forms.ComboBox comboBoxSearchSystem;
+        private System.Windows.Forms.Label labelSearchSystem;
+    }
+}
+
---
+++ 5. Домашняя работа Поноаренко М.Б/HW1-2/FormMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace HomeWork1_2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGetWeather = new System.Windows.Forms.Button();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAPIKEY = new System.Windows.Forms.TextBox();
+            this.dataGridViewInfo = new System.Windows.Forms.DataGridView();
+            this.buttonForecast = new System.Windows.Forms.Button();
+            this.labelCity = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGetWeather
+            // 
+            this.buttonGetWeather.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonGetWeather.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.buttonGetWeather.Location = new System.Drawing.Point(14, 215);
+            this.buttonGetWeather.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonGetWeather.Name = "buttonGetWeather";
+            this.buttonGetWeather.Size = new System.Drawing.Size(178, 38);
+            this.buttonGetWeather.TabIndex = 0;
+            this.buttonGetWeather.Text = "Узнать погоду";
+            this.buttonGetWeather.UseVisualStyleBackColor = false;
+            this.buttonGetWeather.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.ForeColor = System.Drawing.Color.Red;
+            this.textBoxCity.Location = new System.Drawing.Point(196, 80);
+            this.textBoxCity.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(234, 20);
+            this.textBoxCity.TabIndex = 1;
+            this.textBoxCity.Text = "Kaliningrad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 109);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите API key:";
+            // 
+            // textBoxAPIKEY
+            // 
+            this.textBoxAPIKEY.ForeColor = System.Drawing.Color.Red;
+            this.textBoxAPIKEY.Location = new System.Drawing.Point(195, 109);
+            this.textBoxAPIKEY.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxAPIKEY.Name = "textBoxAPIKEY";
+            this.textBoxAPIKEY.Size = new System.Drawing.Size(234, 20);
+            this.textBoxAPIKEY.TabIndex = 4;
+            this.textBoxAPIKEY.Text = "8cbdfff687808c9ab5ffad072111e000";
+            // 
+            // dataGridViewInfo
+            // 
+            this.dataGridViewInfo.AllowUserToAddRows = false;
+            this.dataGridViewInfo.AllowUserToDeleteRows = false;
+            this.dataGridViewInfo.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.dataGridViewInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInfo.Location = new System.Drawing.Point(8, 362);
+            this.dataGridViewInfo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridViewInfo.Name = "dataGridViewInfo";
+            this.dataGridViewInfo.ReadOnly = true;
+            this.dataGridViewInfo.Size = new System.Drawing.Size(955, 148);
+            this.dataGridViewInfo.TabIndex = 7;
+            // 
+            // buttonForecast
+            // 
+            this.buttonForecast.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonForecast.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.buttonForecast.Location = new System.Drawing.Point(254, 215);
+            this.buttonForecast.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonForecast.Name = "buttonForecast";
+            this.buttonForecast.Size = new System.Drawing.Size(175, 38);
+            this.buttonForecast.TabIndex = 8;
+            this.buttonForecast.Text = "Прогноз";
+            this.buttonForecast.UseVisualStyleBackColor = false;
+            this.buttonForecast.Click += new System.EventHandler(this.buttonForecast_Click);
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.BackColor = System.Drawing.Color.White;
+            this.labelCity.Location = new System.Drawing.Point(11, 83);
+            this.labelCity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(165, 13);
+            this.labelCity.TabIndex = 2;
+            this.labelCity.Text = "Введите название города:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = global::HomeWork1_2.Properties.Resources.Прогноз_погоды_3;
+            this.ClientSize = new System.Drawing.Size(976, 534);
+            this.Controls.Add(this.buttonForecast);
+            this.Controls.Add(this.dataGridViewInfo);
+            this.Controls.Add(this.textBoxAPIKEY);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.buttonGetWeather);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormMain";
+            this.Text = "Прогноз погоды......";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetWeather;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAPIKEY;
+        private System.Windows.Forms.DataGridView dataGridViewInfo;
+        private System.Windows.Forms.Button buttonForecast;
+        private System.Windows.Forms.Label labelCity;
+    }
+}
+
